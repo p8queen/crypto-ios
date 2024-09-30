54ee1afdfd55b5f2825ae771c591579eae82edaa
Gustavo Daniel Carmona <p8queen@gmail.com>
2024-09-30 11:19:49 -0300

    Check Color View

--- SwiftfulCrypto/ContentView.swift
+++ SwiftfulCrypto/ContentView.swift
@@ -9,6 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Accent Color")
+                    .foregroundColor(Color.theme.accent)
+                
+                Text("Secondary Text Color")
+                    .foregroundColor(Color.theme.secondaryText)
+
+                Text("Red Color")
+                    .foregroundColor(Color.theme.red)
+
+                Text("Green Color")
+                    .foregroundColor(Color.theme.green)
+            }
+            .padding()
+        }
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
